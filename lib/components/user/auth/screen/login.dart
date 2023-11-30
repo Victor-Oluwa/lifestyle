02fe2294c27718a27987a0aded160d0b1548f697
Victor <gm.oluwafemi@gmail.com>
2023-11-30 05:03:22 +0100

    Added floating text editor

--- lib/components/user/auth/screen/login.dart
+++ lib/components/user/auth/screen/login.dart
@@ -1,31 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart' as x;
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:lifestyle/Common/colors/lifestyle_colors.dart';
 import 'package:lifestyle/Common/widgets/app_constants.dart';
+import 'package:lifestyle/Common/widgets/floating_text_editor.dart';
+import 'package:lifestyle/components/user/auth/screen/signup.dart';
 import 'package:lifestyle/state/providers/actions/provider_operations.dart';
 import 'package:lifestyle/routes-management/lifestyle_routes_names.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import '../../../../../common/widgets/medium_text.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AuthSignInScreen extends ConsumerStatefulWidget {
-  const AuthSignInScreen({super.key});
-  static const String routeName = '/sign-in-creen';
+class LoginScreen extends ConsumerStatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  ConsumerState<AuthSignInScreen> createState() => _AuthSignInScreenState();
+  ConsumerState<LoginScreen> createState() => _AuthSignInScreenState();
 }
 
-class _AuthSignInScreenState extends ConsumerState<AuthSignInScreen> {
+class _AuthSignInScreenState extends ConsumerState<LoginScreen> {
   final _signInFormKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final FocusNode emailFocusNode = FocusNode();
+  final FocusNode passwordFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    emailFocusNode.addListener(() {
+      setState(() {});
+    });
+    passwordFocusNode.addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
 
   @override
   void dispose() {
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+    emailFocusNode.dispose();
+    passwordFocusNode.dispose();
   }
 
   bool _obscureText = true;
@@ -50,165 +67,160 @@ class _AuthSignInScreenState extends ConsumerState<AuthSignInScreen> {
         );
   }
 
-  // Future<void> signIn() async {
-  //   Connectivity().onConnectivityChanged.listen((ConnectivityResult event) {
-  //     if (event == ConnectivityResult.none) {
-  //       x.Get.showSnackbar(const GetSnackBar(
-  //         title: 'ATTENTION',
-  //         message: 'Connect to the internet and try again',
-  //       ));
-  //     } else {
-  //       // await  signInUser();
-  //     }
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFB0A291),
-      // resizeToAvoidBottomInset: false,
-      body: Container(
-        constraints: const BoxConstraints.expand(),
-        decoration: const BoxDecoration(),
-        child: SingleChildScrollView(
-          child: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
               children: [
-                SizedBox(
-                  height: 12.h,
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 4.w),
-                  decoration: const BoxDecoration(),
-                  child: Align(
-                    alignment: Alignment.bottomLeft,
-                    child: MediumText(
-                      font: comorant,
-                      color: Colors.white,
-                      text: 'Log in',
-                      size: 32.sp,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 3.h, left: 5.w, right: 5.w),
-                  child: Form(
-                    key: _signInFormKey,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Please enter your email';
-                            }
-
-                            if (!RegExp(
-                                    r"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$")
-                                .hasMatch(value)) {
-                              return 'Please enter a valid email';
-                            }
-                            return null;
-                          },
-                          keyboardType: TextInputType.emailAddress,
-                          controller: _emailController,
-                          decoration: const InputDecoration(
-                            label: MediumText(text: 'Email'),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 3.h,
-                        ),
-                        TextFormField(
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Please enter your password';
-                            }
-
-                            return null;
-                          },
-                          controller: _passwordController,
-                          obscureText: _obscureText,
-                          decoration: InputDecoration(
-                              label: const MediumText(text: 'Password'),
-                              suffixIcon: IconButton(
-                                  onPressed: _toggleVisibility,
-                                  icon: Icon(_obscureText
-                                      ? Icons.visibility_off
-                                      : Icons.visibility))),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                buildSignInText(),
+                buildSignInForm(),
                 SizedBox(
                   height: 5.h,
                 ),
-                GestureDetector(
-                  onTap: () {
-                    if (_signInFormKey.currentState!.validate()) {
-                      signInUser();
-                    }
-                  },
-                  child: Container(
-                    margin: EdgeInsets.only(top: 5.h),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(5.sp)),
-                    height: 7.h,
-                    width: 90.w,
-                    child: MediumText(
-                      font: comorant,
-                      color: Colors.white,
-                      text: 'Sign In',
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 2.h,
-                ),
-                MediumText(
-                  font: comorant,
-                  text: 'Don\'t have an account?',
-                  color: Colors.white,
-                ),
-                SizedBox(
-                  height: 1.h,
-                ),
-                InkWell(
-                    onTap: () {
-                      x.Get.toNamed(LifestyleRouteName.signUpRoute);
-                      // Navigator.pushNamed(context, AuthScreen.routeName);
-                    },
-                    child: MediumText(
-                      font: comorant,
-                      text: 'Register now?',
-                      color: Colors.white,
-                    ))
-                // GestureDetector(
-                //   onTap: () {
-                //     authService.getUserData(context: context);
-                //   },
-                //   child: Container(
-                //     margin: EdgeInsets.only(top: 5.h),
-                //     alignment: Alignment.center,
-                //     decoration: BoxDecoration(
-                //         color: Colors.grey,
-                //         borderRadius: BorderRadius.circular(15.sp)),
-                //     height: 7.h,
-                //     width: 90.w,
-                //     child: const MediumText(
-                //       text: 'Get user data',
-                //     ),
-                //   ),
-                // ),
+                buildSignInButton(),
               ],
             ),
-          ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MediumText(
+                  font: comorant,
+                  text: 'Don\'t have an account? ',
+                  color: LifestyleColors.white,
+                ),
+                IconButton(
+                  onPressed: () {
+                    x.Get.offAll(() => const SignUpScreen());
+                  },
+                  icon: MediumText(
+                    font: comorant,
+                    text: 'Sign up',
+                    color: LifestyleColors.kTaupeDarkened,
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
+    );
+  }
+
+  Widget buildSignInText() {
+    return Padding(
+      padding: EdgeInsets.only(left: 4.w, top: 13.h),
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: MediumText(
+          font: comorant,
+          color: LifestyleColors.white,
+          text: 'Log in',
+          size: 32.sp,
+        ),
+      ),
+    );
+  }
+
+  GestureDetector buildSignInButton() {
+    return GestureDetector(
+      onTap: () {
+        if (_signInFormKey.currentState!.validate()) {
+          signInUser();
+        }
+      },
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: LifestyleColors.kTaupeDarkened,
+            borderRadius: BorderRadius.circular(5.sp)),
+        height: 7.h,
+        width: 90.w,
+        child: MediumText(
+          font: comorant,
+          color: LifestyleColors.white,
+          text: 'Sign In',
+        ),
+      ),
+    );
+  }
+
+  Container buildSignInForm() {
+    return Container(
+      margin: EdgeInsets.only(top: 3.h, left: 5.w, right: 5.w),
+      child: Form(
+        key: _signInFormKey,
+        child: Column(
+          children: [
+            buildNameTextEditor(),
+            SizedBox(
+              height: 3.h,
+            ),
+            buildPasswordTextEditor(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildPasswordTextEditor() {
+    return FloatingTextEditor(
+      focusNode: passwordFocusNode,
+      controller: _passwordController,
+      label: const MediumText(
+        text: 'Password',
+        color: LifestyleColors.white,
+      ),
+      icon: const Icon(
+        Icons.lock,
+        color: LifestyleColors.white,
+      ),
+      obscureText: _obscureText,
+      suffixIcon: IconButton(
+          onPressed: () {
+            setState(() {
+              _toggleVisibility();
+            });
+          },
+          icon: _obscureText
+              ? const Icon(Icons.visibility_off)
+              : const Icon(Icons.visibility)),
+      validate: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter your password';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget buildNameTextEditor() {
+    return FloatingTextEditor(
+      focusNode: emailFocusNode,
+      controller: _emailController,
+      label: const MediumText(
+        text: 'Email',
+        color: LifestyleColors.white,
+      ),
+      icon: const Icon(
+        Icons.person_2_sharp,
+        color: LifestyleColors.white,
+      ),
+      validate: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter your email';
+        }
+        if (!RegExp(r"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$")
+            .hasMatch(value)) {
+          return 'Please enter a valid email';
+        }
+        return null;
+      },
     );
   }
 }
---
+++ lib/components/user/auth/screen/signup.dart
@@ -0,0 +1,281 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart' as x;
+import 'package:lifestyle/Common/fonts/lifestyle_fonts.dart';
+import 'package:lifestyle/components/user/Documents/screens/privacy_policy.dart';
+import 'package:lifestyle/components/user/auth/screen/login.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+import 'package:lifestyle/Common/colors/lifestyle_colors.dart';
+import 'package:lifestyle/Common/widgets/app_constants.dart';
+import 'package:lifestyle/routes-management/lifestyle_routes_names.dart';
+import 'package:lifestyle/state/providers/actions/provider_operations.dart';
+
+import '../../../../Common/strings/strings.dart';
+import '../../../../Common/widgets/floating_text_editor.dart';
+import '../../../../Common/widgets/medium_text.dart';
+import '../provider/auth_provider.dart';
+
+enum Auth { signIn, signUp }
+
+bool registered = true;
+
+class SignUpScreen extends ConsumerStatefulWidget {
+  const SignUpScreen({super.key});
+  static const String routeName = 'auth-screen';
+
+  @override
+  ConsumerState<SignUpScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends ConsumerState<SignUpScreen> {
+  FocusNode nameFocusNode = FocusNode();
+  FocusNode emailFocusNode = FocusNode();
+  FocusNode passwordFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    emailFocusNode.addListener(() {
+      setState(() {});
+    });
+    nameFocusNode.addListener(() {
+      setState(() {});
+    });
+
+    passwordFocusNode.addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  final _signUpFormKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  bool _obscureText = true;
+
+  void _toggleVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameFocusNode.dispose();
+    emailFocusNode.dispose();
+    passwordFocusNode.dispose();
+    _emailController.dispose();
+    _nameController.dispose();
+    _passwordController.dispose();
+  }
+
+  Future<void> signUpUser() async {
+    final authFunction = ref.read(authFunctionsProvider(context));
+    authFunction.signUpUser(
+        emailController: _emailController,
+        passwordController: _passwordController,
+        nameController: _nameController);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: const Color(0xFFB0A291),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                buildSignUpText(),
+                buildSignUpForm(),
+                SizedBox(
+                  height: 5.h,
+                ),
+                buildSignUpButton(),
+                IconButton(
+                    onPressed: () {
+                      x.Get.to(() => const PrivacyPolicy(),
+                          arguments: privacyPolicyAssetPath);
+                    },
+                    icon: const MediumText(
+                        decoration: TextDecoration.underline,
+                        text: 'Terms and Privacy Policy'))
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MediumText(
+                  font: comorant,
+                  text: 'Already have an account? ',
+                  color: LifestyleColors.white,
+                ),
+                IconButton(
+                  onPressed: () {
+                    x.Get.offAll(() => const LoginScreen());
+                  },
+                  icon: const MediumText(
+                    text: 'Sign in',
+                    color: LifestyleColors.kTaupeDarkened,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding buildSignUpText() {
+    return Padding(
+      padding: EdgeInsets.only(top: 13.h, left: 4.w),
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: MediumText(
+          font: comorant,
+          color: LifestyleColors.white,
+          text: 'Sign Up',
+          size: 32.sp,
+        ),
+      ),
+    );
+  }
+
+  Form buildSignUpForm() {
+    return Form(
+      key: _signUpFormKey,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 4.h,
+            ),
+            buildNameEdittor(),
+            SizedBox(
+              height: 4.h,
+            ),
+            buildEmailEdittor(),
+            SizedBox(
+              height: 4.h,
+            ),
+            buildPasswordEdittor(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildNameEdittor() {
+    return FloatingTextEditor(
+      focusNode: nameFocusNode,
+      controller: _nameController,
+      label: const MediumText(
+        text: 'Name',
+        color: LifestyleColors.white,
+      ),
+      icon: const Icon(
+        Icons.person_2_sharp,
+        color: LifestyleColors.white,
+      ),
+      validate: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter your name';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget buildEmailEdittor() {
+    return FloatingTextEditor(
+      focusNode: emailFocusNode,
+      controller: _emailController,
+      label: const MediumText(
+        text: 'Email',
+        color: LifestyleColors.white,
+      ),
+      icon: const Icon(
+        Icons.email,
+        color: LifestyleColors.white,
+      ),
+      validate: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter your email';
+        }
+        if (!RegExp(r"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$")
+            .hasMatch(value)) {
+          return 'Please enter a valid email';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget buildPasswordEdittor() {
+    return FloatingTextEditor(
+      obscureText: _obscureText,
+      focusNode: passwordFocusNode,
+      controller: _passwordController,
+      label: const MediumText(
+        text: 'Password',
+        color: LifestyleColors.white,
+      ),
+      icon: const Icon(
+        Icons.lock,
+        color: LifestyleColors.white,
+      ),
+      suffixIcon: IconButton(
+        onPressed: () {
+          setState(() {
+            _toggleVisibility();
+          });
+        },
+        icon: _obscureText
+            ? const Icon(Icons.visibility_off)
+            : const Icon(Icons.visibility),
+      ),
+      validate: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter a password';
+        }
+        _passwordController.clear();
+        return null;
+      },
+    );
+  }
+
+  Padding buildSignUpButton() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 5.w),
+      child: GestureDetector(
+        onTap: (() {
+          if (_signUpFormKey.currentState!.validate()) {
+            signUpUser();
+          }
+        }),
+        //Conditionally main sign in or Sign up button
+        child: Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: LifestyleColors.kTaupeDarkened,
+                borderRadius: BorderRadius.circular(5.sp)),
+            height: 7.h,
+            width: 90.w,
+            child: MediumText(
+              font: comorant,
+              color: LifestyleColors.white,
+              text: 'Sign Up',
+            )),
+      ),
+    );
+  }
+}
